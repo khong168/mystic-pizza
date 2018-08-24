6485fa10919f3584ddef2d12f13777697376ccde
hingyi khong <hingyi@gmail.com>
2018-08-24 11:28:01 -0700

    add veggies menu and function to total

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,62 @@
 // Back End
-function PizzaOrder(size, toppings) {
+function PizzaOrder(size, toppings, veggies) {
   this.size = size;
   this.toppings = toppings;
+  this.veggies = veggies;
   this.price = 10;
 }
 
 PizzaOrder.prototype.pizzaTotal = function() {
-  if (this.size === "small") {
-    this.price = this.price +2;
-  }
-  if (this.size === "medium") {
-    this.price = this.price +4;
-  }
-  if (this.size === "large") {
-    this.price = this.price +6;
-  }
-
-  if (this.toppings.length >= 1) {
-    for (var i = 0; i < this.toppings.length; i++) {
-      this.price += 1;
+    if (this.size === "small") {
+      this.price = this.price + 2;
     }
-    this.toppings = this.toppings.join(', ');
-  }
+    if (this.size === "medium") {
+      this.price = this.price + 4;
+    }
+    if (this.size === "large") {
+      this.price = this.price + 6;
+    }
 
-  return this.size + ", " + this.toppings + ", $" + this.price;
-};
-  // Front End
-  $(document).ready(function() {
-    $("form#size").submit(function(event) {
-      event.preventDefault();
-      var inputtedSize = $("#pizzaSize").val();
-      console.log(inputtedSize);
-      var inputtedToppings = [];
-      $.each($("input[name='toppings']:checked"), function() {
-        inputtedToppings.push($(this).val());
-      console.log(inputtedToppings);
+    if (this.toppings.length >= 1) {
+      for (var i = 0; i < this.toppings.length; i++) {
+        this.price += 2;
+      }
+      this.toppings = this.toppings.join(', ');
+    }
+
+    if (this.veggies.length >= 1) {
+      for (var j = 0; j < this.veggies.length; j++) {
+        this.price += 1;
+      }
+      this.veggies = this.veggies.join(', ');
+    }
+
+
+      return this.size + ", " + this.toppings + ", " + this.veggies + ", $" + this.price;
+    };
+    // Front End
+    $(document).ready(function() {
+      $("form#size").submit(function(event) {
+        event.preventDefault();
+        var inputtedSize = $("#pizzaSize").val();
+        console.log(inputtedSize);
+        var inputtedToppings = [];
+        $.each($("input[name='toppings']:checked"), function() {
+          inputtedToppings.push($(this).val());
+          console.log(inputtedToppings);
+        });
+        var inputtedVeggies = [];
+        $.each($("input[name='veggies']:checked"), function() {
+          inputtedVeggies.push($(this).val());
+          console.log(inputtedVeggies);
+        });
+        var newPizzaOrder = new PizzaOrder(inputtedSize, inputtedToppings, inputtedVeggies);
+        console.log(newPizzaOrder);
+        var pizzaDisplay = newPizzaOrder.pizzaTotal();
+        console.log(pizzaDisplay);
+        $(".results").text(pizzaDisplay);
+
+
+
+      });
     });
-      var newPizzaOrder = new PizzaOrder(inputtedSize, inputtedToppings);
-      console.log(newPizzaOrder);
-      var pizzaDisplay = newPizzaOrder.pizzaTotal();
-      console.log(pizzaDisplay);
-      $(".results").text(pizzaDisplay);
-
-
-
-    });
-  });
