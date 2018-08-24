6689b43c1107a8e2b9f94e0da41942c4d9e1bea9
hingyi khong <hingyi@gmail.com>
2018-08-24 09:48:41 -0700

    create new pizza constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
-$(document).ready(function() {
-  $("form#size").submit(function(event) {
-    event.preventDefault();
-    var inputtedSize = $("#pizzaSize").val();
-    console.log(inputtedSize);
+// Back End
+function PizzaOrder(size, toppings) {
+  this.size = size;
+  this.toppings = toppings;
+  this.price = 10;
+}
+  // Front End
+  $(document).ready(function() {
+    $("form#size").submit(function(event) {
+      event.preventDefault();
+      var inputtedSize = $("#pizzaSize").val();
+      console.log(inputtedSize);
+      var inputtedToppings = [];
+      $.each($("input[name='toppings']:checked"), function() {
+        inputtedToppings.push($(this).val());
+      console.log(inputtedToppings);
+      var newPizza = new PizzaOrder(inputtedSize, inputtedToppings);
+      console.log(newPizza);
+      });
+    });
   });
-});
