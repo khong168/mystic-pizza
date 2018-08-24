0f086a9e05c1341aacd35ef003c0e6244426ecde
hingyi khong <hingyi@gmail.com>
2018-08-24 10:12:01 -0700

    add total on size input

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,33 @@ function PizzaOrder(size, toppings) {
   this.toppings = toppings;
   this.price = 10;
 }
+
+PizzaOrder.prototype.pizzaTotal = function() {
+  if (this.size === "small") {
+    this.price = this.price +2;
+  }
+  if (this.size === "medium") {
+    this.price = this.price +4;
+  }
+  if (this.size === "large") {
+    this.price = this.price +6;
+  }
+  return this.price;
+//   if (this.age === "senior") {
+//     this.price -= 2;
+//   }
+//   if (this.name === "StarWars") {
+//     this.price -= 2;
+//   }
+//   if (this.snacks.length >= 1) {
+//     for (var i = 0; i < this.snacks.length; i++) {
+//       this.price += 1;
+//     }
+//     this.snacks = this.snacks.join(', ');
+//     // console.log(this.snacks);
+// }
+//   return this.name + ", " + this.time + ", " + this.age + ", " + this.price + ", " + this.snacks;
+};
   // Front End
   $(document).ready(function() {
     $("form#size").submit(function(event) {
@@ -14,8 +41,12 @@ function PizzaOrder(size, toppings) {
       $.each($("input[name='toppings']:checked"), function() {
         inputtedToppings.push($(this).val());
       console.log(inputtedToppings);
+    });
       var newPizza = new PizzaOrder(inputtedSize, inputtedToppings);
       console.log(newPizza);
-      });
+      var pizzaDisplay = newPizza.pizzaTotal();
+      console.log(pizzaDisplay);
+
+
     });
   });
